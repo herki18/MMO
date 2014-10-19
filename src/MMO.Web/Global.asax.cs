7c43145b6daf80a8e6286b695487361a52078cc3
herki18 <herki77@gmail.com>
2014-10-19 23:17:55 +0300

    test

--- src/MMO.Web/Global.asax.cs
+++ src/MMO.Web/Global.asax.cs
@@ -29,8 +29,8 @@ namespace MMO.Web
 
         public void SetUpSerilog() {
             Log.Logger = new LoggerConfiguration()
-                .WriteTo.Seq("http://herki.cloudapp.net:5341")
-                .CreateLogger();
+                      .WriteTo.File(@"C:\Loggs")
+                      .CreateLogger();
 
             //// Standard .NET format string, useful if you're migrating from another logger
             //Log.Information("Starting up on {0} with {1} bytes allocated", Environment.MachineName, Environment.WorkingSet);
